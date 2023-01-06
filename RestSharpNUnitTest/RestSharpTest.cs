7cda974b14d33060c7720bce19a83e46bb86eae2
preetham-coupa <preetham.p-m@coupa.com>
2023-01-06 17:02:07 +0530

    Added Pass Test Case

--- RestSharpNUnitTest/RestSharpTest.cs
+++ RestSharpNUnitTest/RestSharpTest.cs
@@ -7,11 +7,21 @@ public class RestSharpTest
     private string getUrl = "https://api.publicapis.org/entries";
 
     [Test]
-    public void Test1()
+    public void TestingConnection()
     {
         RestClient restClient = new RestClient();
         RestRequest restRequest= new RestRequest(getUrl);
         var restResponse = restClient.Get(restRequest);
         Assert.AreEqual(true, restResponse.IsSuccessful);
+    }
+
+    [Test]
+    public void TestingResponse() 
+    {
+        RestClient restClient = new RestClient();
+        RestRequest restRequest = new RestRequest(getUrl);
+        var restResponse = restClient.Get(restRequest);
+        var desired = "OK";
+        Assert.AreEqual(restResponse.StatusCode.ToString(), desired);
     }
 }
