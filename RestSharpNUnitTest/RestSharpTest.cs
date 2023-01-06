1efa8a2d57871d51454e6be0f4b34c62cd7030d4
preetham-coupa <preetham.p-m@coupa.com>
2023-01-06 17:06:35 +0530

    Added Fail Test Case

--- RestSharpNUnitTest/RestSharpTest.cs
+++ RestSharpNUnitTest/RestSharpTest.cs
@@ -6,12 +6,22 @@ public class RestSharpTest
 {
     private string getUrl = "https://api.publicapis.org/entries";
 
-    [Test]
-    public void Test1()
+   [Test]
+    public void TestingConnection()
     {
         RestClient restClient = new RestClient();
         RestRequest restRequest= new RestRequest(getUrl);
         var restResponse = restClient.Get(restRequest);
         Assert.AreEqual(true, restResponse.IsSuccessful);
+    }
+
+    [Test]
+    public void TestingResponse() 
+    {
+        RestClient restClient = new RestClient();
+        RestRequest restRequest = new RestRequest(getUrl+"aa");
+        var restResponse = restClient.Get(restRequest);
+        var desired = "NotFound ";
+        Assert.AreEqual(restResponse.StatusCode.ToString(), desired);
     }
 }
